82dcade604574c5bb9f881eca397e9c842514584
ekrem35 <anliekrem35@gmail.com>
2023-11-22 21:27:14 +0300

    add clarification text to welcome screen

--- app/screens/Onboarding/Welcome.tsx
+++ app/screens/Onboarding/Welcome.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import {View, StatusBar, StyleSheet, Text, Image} from 'react-native';
 
 import Container from './components/Container';
+import Button from 'components/Button';
 
 import PlantImgSVG from './assets/plant_camera_line.image.svg';
 
 import spray_image from './assets/object_spray.png';
 import water_image from './assets/object_water.png';
 import sun_image from './assets/object_sun.png';
-import Button from 'components/Button';
 
 export default function Onboarding() {
   return (
@@ -40,6 +40,19 @@ export default function Onboarding() {
         </View>
 
         <Button>Get Started</Button>
+
+        <View style={styles.policyTextContainer}>
+          <Text style={styles.policyText}>
+            By tapping next, you are agreeing to PlantID{' '}
+            <Text onPress={() => true} style={styles.clickableText}>
+              Terms of Use
+            </Text>{' '}
+            &{' '}
+            <Text onPress={() => true} style={styles.clickableText}>
+              Privacy Policy.
+            </Text>
+          </Text>
+        </View>
       </Container>
     </View>
   );
@@ -75,7 +88,11 @@ const styles = StyleSheet.create({
     marginTop: 24,
     alignItems: 'center',
   },
-  objectImagesContainer: {width: 300, height: 400, position: 'absolute'},
+  objectImagesContainer: {
+    width: 300,
+    height: 400,
+    position: 'absolute',
+  },
   sprayImg: {
     position: 'absolute',
     left: 0,
@@ -89,5 +106,23 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 20,
     right: 70,
+  },
+  policyTextContainer: {
+    alignItems: 'center',
+  },
+  policyText: {
+    fontFamily: 'Rubik',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 11,
+    lineHeight: 15,
+    textAlign: 'center',
+    letterSpacing: 0.07,
+    color: 'rgba(89, 113, 101, 0.7)',
+    marginTop: 17,
+    maxWidth: '75%',
+  },
+  clickableText: {
+    textDecorationLine: 'underline',
   },
 });
