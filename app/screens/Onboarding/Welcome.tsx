69a2227f9eb5f0261d5f6f76b25e0292fc1c2634
ekrem35 <anliekrem35@gmail.com>
2023-11-22 14:53:15 +0300

    add onboarding main image

--- app/screens/Onboarding/Welcome.tsx
+++ app/screens/Onboarding/Welcome.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-import {View, StatusBar, StyleSheet, Text} from 'react-native';
+import {View, StatusBar, StyleSheet, Text, Image} from 'react-native';
 
 import Container from './components/Container';
+
+import PlantImgSVG from './assets/plant_camera_line.image.svg';
+
+import spray_image from './assets/object_spray.png';
+import water_image from './assets/object_water.png';
+import sun_image from './assets/object_sun.png';
 
 export default function Onboarding() {
   return (
@@ -10,10 +16,7 @@ export default function Onboarding() {
       <StatusBar barStyle="dark-content" />
 
       <Container>
-        <View
-          style={{
-            marginTop: 12,
-          }}>
+        <View style={styles.headerTextContainer}>
           <Text style={styles.title}>
             Welcome to <Text style={styles.titleBold}>PlantApp</Text>
           </Text>
@@ -22,12 +25,27 @@ export default function Onboarding() {
             Identify more than 3000+ plants and 88% accuracy.
           </Text>
         </View>
+
+        <View style={styles.plantImgContainer}>
+          <View style={styles.objectImagesContainer}>
+            <Image source={spray_image} style={styles.sprayImg} />
+
+            <Image source={sun_image} style={styles.sunImg} />
+
+            <Image source={water_image} style={styles.waterImg} />
+          </View>
+
+          <PlantImgSVG />
+        </View>
       </Container>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  headerTextContainer: {
+    marginTop: 12,
+  },
   title: {
     fontFamily: 'Rubik',
     fontStyle: 'normal',
@@ -49,5 +67,24 @@ const styles = StyleSheet.create({
     lineHeight: 22,
     color: 'rgba(19, 35, 27, 0.7)',
     marginTop: 8,
+  },
+  plantImgContainer: {
+    marginTop: 24,
+    alignItems: 'center',
+  },
+  objectImagesContainer: {width: 300, height: 400, position: 'absolute'},
+  sprayImg: {
+    position: 'absolute',
+    left: 0,
+  },
+  sunImg: {
+    position: 'absolute',
+    right: 30,
+    top: 40,
+  },
+  waterImg: {
+    position: 'absolute',
+    bottom: 20,
+    right: 70,
   },
 });
