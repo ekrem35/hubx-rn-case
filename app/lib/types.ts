72e0e0f8a8bc7c8ce38c36a1338bbb9530877941
ekrem35 <anliekrem35@gmail.com>
2023-11-24 15:39:18 +0300

    add types of response items

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,3 +1,28 @@
 export interface INavigation {
   navigate: (route: string, params?: any) => void;
 }
+
+export interface IQuestion {
+  id: number;
+  title: string;
+  subtitle: string;
+  image_uri: string;
+  order: number;
+  uri: string;
+}
+
+export interface ICategory {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  title: string;
+  image: {
+    id: number;
+    name: Date;
+    url: 'https://cms-cdn.plantapp.app/6_edbcc6988a/6_edbcc6988a.png';
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
