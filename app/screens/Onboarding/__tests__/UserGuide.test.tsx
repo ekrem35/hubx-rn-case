4eeb409821435ca5badbf315855b98a85f8197e9
ekrem35 <anliekrem35@gmail.com>
2023-11-25 14:33:06 +0300

    test: update userGuide screen test

--- app/screens/Onboarding/__tests__/UserGuide.test.tsx
+++ app/screens/Onboarding/__tests__/UserGuide.test.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import {cleanup} from '@testing-library/react-native';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
 
+import {cleanup} from '@testing-library/react-native';
 import renderer from 'react-test-renderer';
 
 import UserGuide from '../UserGuide';
+import storageReducer from '../../../state/slices/storage';
 
 afterEach(cleanup);
 
 describe('testing UserGuide screen', () => {
   it('should renders correctly', () => {
+    const mockStore = configureStore({
+      reducer: {
+        storage: storageReducer,
+      },
+    });
+
     const testRenderer = renderer.create(
-      <UserGuide
-        navigation={{
-          navigate: jest.fn(),
-        }}
-      />,
+      <Provider store={mockStore}>
+        <UserGuide
+          navigation={{
+            navigate: jest.fn(),
+          }}
+        />
+      </Provider>,
     );
     const testInstance = testRenderer.root;
 
